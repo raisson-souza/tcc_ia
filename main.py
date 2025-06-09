59f7addc914d76f9f910b8a8eee3ec754b6aa075
Raisson Souza <raissonrai@gmail.com>
2025-06-08 21:12:46 -0300

    Att

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import os
 
 app = Flask(__name__)
 
-PDF_DIR = os.path.dirname(os.path.abspath(__file__))
-PDF_FILENAME = "./doc.pdf"
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-@app.route("/download", methods=["GET"])
-def download_pdf():
+def download_file(file_name):
     try:
-        return send_from_directory(PDF_DIR, PDF_FILENAME, as_attachment=True)
+        return send_from_directory(BASE_PATH, f"./files/{ file_name }", as_attachment=True)
     except FileNotFoundError:
         return {"error": "Arquivo não encontrado."}, 404
+
+@app.route("/download/freud", methods=["GET"])
+def download_freud():
+    return download_file("freud.pdf")
+
+@app.route("/download/change", methods=["GET"])
+def download_change():
+    return download_file("change.pdf")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
