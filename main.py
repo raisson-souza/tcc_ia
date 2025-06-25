c19f5433fea28cb2f3541faf20b6a0d3c1296da5
Raisson Souza <raissonrai@gmail.com>
2025-06-24 22:40:16 -0300

    - Rota para tb Marco; - Pré rotas tcc.

--- main.py
+++ main.py
@@ -11,13 +11,25 @@ def download_file(file_name):
     except FileNotFoundError:
         return {"error": "Arquivo não encontrado."}, 404
 
-@app.route("/download/freud", methods=["GET"])
+@app.route("/download/psicanalise", methods=["GET"])
 def download_freud():
     return download_file("freud.pdf")
 
 @app.route("/download/change", methods=["GET"])
 def download_change():
     return download_file("change.pdf")
+
+@app.route("/download/marco", methods=["GET"])
+def download_marco():
+    return download_file("marco.csv")
+
+@app.route("/download/ontopsicologia", methods=["GET"])
+def download_ontopsicologia():
+    return download_file("ontopsicologia.pdf")
+
+@app.route("/download/psicanalise_ontopsicologia", methods=["GET"])
+def download_psicanalise_ontopsicologia():
+    return download_file("psicanalise_ontopsicologia.pdf")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
