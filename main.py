25850cfeaee62bbe7fa7e24866a355deaa91d394
Raisson Souza <raissonrai@gmail.com>
2025-07-04 11:54:38 -0300

    removido código nao utilizado.

--- main.py
+++ main.py
@@ -21,11 +21,6 @@ def download_freud():
 def download_psicanalise():
     return download_file("psicanalise.pdf")
 
-# MARCO
-@app.route("/download/marco", methods=["GET"])
-def download_marco():
-    return download_file("marco.pdf")
-
 # ONTOPSICOLOGIA
 @app.route("/download/ontopsicologia", methods=["GET"])
 def download_ontopsicologia():
