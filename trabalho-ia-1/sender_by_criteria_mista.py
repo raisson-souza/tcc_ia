082f6b27fe95e55aef2e7699cbf8ebb2dcc8018e
Raisson Souza <raissonrai@gmail.com>
2025-07-04 11:53:51 -0300

    Adicionados scripts do trabalho de IA 1.

---
+++ trabalho-ia-1/sender_by_criteria_mista.py
@@ -0,0 +1,20 @@
+# Para workflow com interpretação MISTA por critério (pausada)
+
+import requests
+
+url = 'http://localhost:5678/webhook-test/criteria-mista'
+
+USER_DREAM = "Estava dirigindo um carro completamente desgovernado sem controle nenhum, mexer o volante não funcionava."
+DREAM_CONTEXT = "Eu não tenho carro nem carteira de motorista, não sei dirigir."
+USER_CONTEXT = "O paciente está sobrecarregado com TCC da faculdade, ele estuda e trabalha muito."
+
+body = {
+    "user_dream": USER_DREAM,
+    "dream_context": DREAM_CONTEXT,
+    "user_context": USER_CONTEXT,
+}
+
+response = requests.post(url, json=body)
+
+print("Status:", response.status_code)
+print(response.json())
